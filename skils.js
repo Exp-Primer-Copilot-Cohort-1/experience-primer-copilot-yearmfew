7eb482bc50cee077c1ff4c2b755ba901754f2d50
Birol Yilmaz <brlylmz23@gmail.com>
2024-03-03 11:15:29 +0100

    skills commit

--- skils.js
+++ skils.js
@@ -1,3 +1,22 @@
 function calculateNumbers(var1, var2) {
 
 }
+
+function sumNumbers(a, b) {
+
+}
+
+function multiplyNumbers(a, b) {
+
+}
+
+
+/** divide Numbers */
+function divideNumbers(a, b) {
+    a / b;
+}
+
+/** subtract Numbers */
+function subtractNumbers(a, b) {
+    a - b;
+}
